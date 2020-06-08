7744501a44ab101ba9f6cae376d875685c334303
robertotics4 <robertooliveiragp@gmail.com>
2020-06-08 12:37:20 -0300

    configuring project

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        message: 'Ahay'
+    });
+});
+
+app.listen(3333);
